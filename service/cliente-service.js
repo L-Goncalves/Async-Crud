94525f90c97309b3b716edc550fd3c69dcf2e937
Lucas Gonçalves <ldgoncalvs@gmail.com>
2021-02-17 02:44:48 -0300

    Criando novos Clientes com POST

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -4,7 +4,26 @@
    })
 
 };
+
+const CriaCliente = (nome, email) =>{
+   return fetch(`http://localhost:3000/profile`,{
+     method: 'POST',
+     headers: {
+        'Content-Type': 'application/json'
+     },
+     body: JSON.stringify({
+      nome: nome,
+      email: email
+     })
+   })
+   .then( res =>{
+      res.body
+   })
+
+
+
+}
    
 export const clienteService = {
-   listaClientes
+   listaClientes, CriaCliente
 }
