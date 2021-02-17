b611c84696c2dcdbdc594850c671011c75ba9c23
Lucas Gonçalves <ldgoncalvs@gmail.com>
2021-02-17 02:10:58 -0300

    First Commit

---
+++ service/cliente-service.js
@@ -0,0 +1,10 @@
+ const listaClientes = () =>{
+   return fetch(`http://localhost:3000/profile`).then(res => {
+      return res.json();
+   })
+
+};
+   
+export const clienteService = {
+   listaClientes
+}
